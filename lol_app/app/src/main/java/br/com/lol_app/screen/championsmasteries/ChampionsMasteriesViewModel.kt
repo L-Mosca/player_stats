be3e862c34f767b66b679718ddf4ddccd22902fe
L-Mosca <lucasbvmosca@gmail.com>
2023-11-20 19:07:30 -0300

    Feature: Add toggle visibility.

--- lol_app/app/src/main/java/br/com/lol_app/screen/championsmasteries/ChampionsMasteriesViewModel.kt
+++ lol_app/app/src/main/java/br/com/lol_app/screen/championsmasteries/ChampionsMasteriesViewModel.kt
@@ -1,7 +1,11 @@
 package br.com.lol_app.screen.championsmasteries
 
+import android.content.Context
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import br.com.lol_app.R
 import br.com.lol_app.base.BaseViewModel
 import br.com.lol_app.domain.model.champions.ChampionBaseData
 import br.com.lol_app.domain.repositories.champion.ChampionRepositoryContract
@@ -15,12 +19,30 @@ class ChampionsMasteriesViewModel @Inject constructor(private val championReposi
     private val _championsMasteries = MutableLiveData<List<ChampionBaseData>?>()
     val championsMasteries: LiveData<List<ChampionBaseData>?> get() = _championsMasteries
 
+    val slideUpAnimation: LiveData<Pair<Animation, Animation>> get() = _slideUpAnimation
+    private val _slideUpAnimation = MutableLiveData<Pair<Animation, Animation>>()
+
+    val slideDownAnimation: LiveData<Pair<Animation, Animation>> get() = _slideDownAnimation
+    private val _slideDownAnimation = MutableLiveData<Pair<Animation, Animation>>()
+
     fun fetchChampionsMasteries(summonerId: String) {
         defaultLaunch {
             val response = championRepository.fetchChampionsMasteries(summonerId)
             if (!response.isNullOrEmpty()) {
                 _championsMasteries.postValue(response)
             }
+        }
+    }
+
+    fun setListAnimation(isVisible: Boolean, context: Context) {
+        if (isVisible) {
+            val listAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_up)
+            val iconAnimation = AnimationUtils.loadAnimation(context, R.anim.rotate_up_to_down)
+            _slideUpAnimation.postValue(Pair(listAnimation, iconAnimation))
+        } else {
+            val listAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_down)
+            val iconAnimation = AnimationUtils.loadAnimation(context, R.anim.rotate_down_to_up)
+            _slideDownAnimation.postValue(Pair(listAnimation, iconAnimation))
         }
     }
 }
